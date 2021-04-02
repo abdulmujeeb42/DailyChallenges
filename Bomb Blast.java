a8200e3b4e6a635cb621784b1d8080d559b11e1e
abdulmujeeb42 <78905978+abdulmujeeb42@users.noreply.github.com>
2021-04-02 08:20:00 +0530

    Update Bomb Blast.java

--- Bomb Blast.java
+++ Bomb Blast.java
@@ -1,3 +1,57 @@
+
+A matrix of size Rit containing positive integer values is passed as the input An integer value which s a multiple of S is a bomb and destroy our adjacent cells in the following order left right top and bottom. But if the adjacent cell is water the integer value will be a multiple of 3), then the bomb will stop destroying the adjacent cels Ha cell is destroyed then the value of that all oeccines 0 After the bomb blasted the the delle esimees Ole the cell value both multiple and then consider that as water Finally the cream must in the Values in the atrix 35 the sun
+
+
+Bample Input/Output 1:
+
+Input
+
+33
+
+555 61 20
+
+1
+
+156 10
+
+Output 000
+
+C00
+
+15 60
+
+Explanation
+
+After the bomb 10 in the last row is triggered the left value is 6 and is water So the explosion stops Hence the matrix state is
+
+555
+
+61 20
+
+15 60
+
+Now 15 s both a multiple of 5 and 3. Hence it is considered as wa
+
+in the 2nd row from the bottom 20 is a bomb and it destroys the left cell with right cell with the top cal
+
+with 5. The bottom cell water and hence remains unchanged. Hence the matro state is
+
+505
+
+000
+
+15 60
+
+TH the 3 row from the bottom two bombs with the values 5) explode and hence the fira state is
+
+400
+
+900
+
+15 60
+
+
+
 import java.util.*;
 public class Hello {  
     static int r,c;
